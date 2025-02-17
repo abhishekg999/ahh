c849e9b0a231eb1561bf27c72441cdc068245806
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2025-02-17 01:09:18 -0800

    fix: node_env wierd

--- ahh-binary/src/constants/main.ts
+++ ahh-binary/src/constants/main.ts
@@ -1,3 +1,13 @@
-export const DEV = process.env.NODE_ENV === "development";
-export const AHH_WEBHOOK_URL = process.env.AHH_WEBHOOK_URL ?? "https://cli.ahh.bet/webhook";
-export const AHH_DOCS_URL = process.env.AHH_DOCS_URL ?? "https://cli.ahh.bet/docs";
+export const DEV = process.env.IS_LOCAL === "1";
+
+if (DEV) console.log("Running in development mode.");
+
+export const AHH_WEBHOOK_URL =
+  process.env.AHH_WEBHOOK_URL ?? "https://cli.ahh.bet/webhook";
+  
+export const AHH_DOCS_URL =
+  process.env.AHH_DOCS_URL ?? "https://cli.ahh.bet/docs";
+
+export const WORKSPACE_DOWNLOAD_PATH = DEV
+  ? "https://localhost:3000/workspaces"
+  : "https://cli.ahh.bet/workspaces";
--- ahh-binary/src/workspace/main.ts
+++ ahh-binary/src/workspace/main.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { $ } from "bun";
 import { exists, HOME_DIR, mkdirAlways } from "../utils/fs";
+import { WORKSPACE_DOWNLOAD_PATH } from "../constants/main";
 
 const WORKSPACE_DIR = path.join(HOME_DIR, "workspaces");
 const WORKSPACE_CACHE_DIR = path.join(WORKSPACE_DIR, "cache");
@@ -8,39 +9,20 @@ const WORKSPACE_CACHE_DIR = path.join(WORKSPACE_DIR, "cache");
 await mkdirAlways(WORKSPACE_DIR);
 await mkdirAlways(WORKSPACE_CACHE_DIR);
 
-const DOWNLOAD_PATH =
-  process.env.NODE_ENV === "development"
-    ? "https://localhost:3000/workspaces"
-    : "https://cli.ahh.bet/workspaces";
-
 export async function downloadAndCacheWorkspace(workspaceId: string) {
     const workspaceCachePath = path.join(WORKSPACE_CACHE_DIR, workspaceId);
     await mkdirAlways(workspaceCachePath);
     
-    const downloadPath = `${DOWNLOAD_PATH}/${workspaceId}/download.tar.gz`;
+    const downloadPath = `${WORKSPACE_DOWNLOAD_PATH}/${workspaceId}/download.tar.gz`;
     await $`curl -sL ${downloadPath} | tar -xz -C ${workspaceCachePath}`;
 }
 
-/**
- * Loads a workspace from cache to the wanted loc.
- */
 async function loadWorkspace(workspaceId: string, loc: string) {
     const workspaceCachePath = path.join(WORKSPACE_CACHE_DIR, workspaceId);
     await mkdirAlways(loc);
     await $`cp -r ${workspaceCachePath}/* ${loc}`;
 }
 
-/**
- * Check if workspaceId is in WORKSPACE_CACHE_DIR
- * If it is: 
-    - Continue
- * If it is not:
- *   - Call downloadAndCacheWorkspace(workspaceId)
- * 
- *  - Copy the workspace to loc
- *  - If loc is null: copy it to ./workspaceId/*
- *  - If loc is not null: mkdir -p loc && copy it to loc
- */
 export async function initWorkspace(
   workspaceId: string,
   loc?: string
